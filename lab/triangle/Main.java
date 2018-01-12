e594b4a4e0fe503adda3a395f74a4326ee0e6d8d
tcaer <tino.caer@me.com>
2018-01-12 08:35:25 -0500

    12/01/18

--- lab/triangle/Main.java
+++ lab/triangle/Main.java
@@ -4,8 +4,9 @@ public class Main {
 		Triangle et = new EquilateralTriangle(12);
 		Triangle rt = new RightTriangle(12);
 
-		System.out.println(rt.getArea());
-		System.out.println(et.getArea());
+		System.out.println((2 * et.getArea())/et.getPerimeter());
+		System.out.println((2 * rt.getArea())/rt.getPerimeter());
+		System.out.println("The right triangle can hold a bigger circle");
 	}
 
 }
